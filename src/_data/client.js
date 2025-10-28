97c72a6afafb9b562b7e52d6420ced7168305115
biokram <mark.elmore87@gmail.com>
2025-10-28 12:10:11 -0500

    initial commit of new completed code

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,24 @@
 module.exports = {
     name: "Gateway Cornerstone Web Design",
-    email: "mail@gatewaycornerstone.com",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    email: "gatewaycornerstoneweb@gmail.com",
+    phoneForTel: "+13096346106",
+    phoneFormatted: "(309) 634-6106",
     address: {
         lineOne: "P.O. Box 12345",
         //! lineTwo: "",
-        city: "St. Louis",
-        state: "MO",
-        zip: "63101",
+        city: "Swansea",
+        state: "IL",
+        zip: "62226",
         country: "US",
         mapLink: "https://maps.app.goo.gl/iKoPf1B6CoHVoWNi6",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
+        facebook: "https://www.facebook.com/people/Gateway-Cornerstone-Web-Design/61582680824948/",
         instagram: "https://www.instagram.com/",
+        googleBusiness: "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.gatewaycornerstone.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
