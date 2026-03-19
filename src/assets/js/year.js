3ac46f351bde5fbc2da6cd53a981680502fe5884
biokram <mark.elmore87@gmail.com>
2026-03-19 11:36:52 -0500

    fixed copyright footer

--- src/assets/js/year.js
+++ src/assets/js/year.js
@@ -2,10 +2,7 @@
 function updateCopyrightYear() {
     const currentYear = new Date().getFullYear();
     document.getElementById('dynamic-copyright').textContent = `&copy; ${currentYear}`;
+    window.addEventListener('load', updateCopyrightYear);
   }
 
-  // Call the function immediately
   updateCopyrightYear();
-
-  // Optionally, you can add an event listener to update the year whenever the page is loaded
-  window.addEventListener('load', updateCopyrightYear);
